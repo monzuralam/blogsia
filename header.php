2ae8c7e65f07736599fe50176d31befcc6147ff4
monzuralam <monzur707@gmail.com>
2022-06-13 17:57:43 +0600

    logo dynamic

--- header.php
+++ header.php
@@ -24,7 +24,38 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'blogsia' ); ?></a>
-
+	<!-- Navigation-->
+	<div class="bg-primary">
+		<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+			<div class="container px-4 px-lg-5">
+				<a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
+					<?php
+											
+						if( has_custom_logo() ){
+							if( function_exists( 'the_custom_logo' ) ){
+								the_custom_logo();
+							}
+						}else{
+							echo esc_html__( get_bloginfo('name'), 'blogsia' ) ;
+						}
+						
+					?>
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<?php echo esc_html__( 'Menu', 'blogsia' ); ?>
+					<i class="fas fa-bars"></i>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto py-4 py-lg-0">
+						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
+						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
+						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
+						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
