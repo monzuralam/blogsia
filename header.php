ba5eaea7ee44f889490d79292c4a393df4fcecc2
monzuralam <monzur707@gmail.com>
2022-06-13 19:00:33 +0600

    menu dynamic

--- header.php
+++ header.php
@@ -25,66 +25,43 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'blogsia' ); ?></a>
 	<!-- Navigation-->
-	<div class="bg-primary">
+	<header id="masthead" class="site-header">
 		<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
 			<div class="container px-4 px-lg-5">
-				<a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
-					<?php
-											
-						if( has_custom_logo() ){
-							if( function_exists( 'the_custom_logo' ) ){
-								the_custom_logo();
-							}
-						}else{
-							echo esc_html__( get_bloginfo('name'), 'blogsia' ) ;
+				<?php
+
+					if( has_custom_logo() ){
+						if( function_exists( 'the_custom_logo' ) ){
+							the_custom_logo();
 						}
-						
-					?>
-				</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-					<?php echo esc_html__( 'Menu', 'blogsia' ); ?>
+					}else{
+						echo '<a class="navbar-brand" href="'. esc_url(home_url()).'">';
+						echo esc_html__( get_bloginfo('name'), 'blogsia' ) ;
+						echo '</a>';
+					}
+					
+				?>
+			
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<?php echo esc_html__( 'Menu', 'blogsia' ); ?>	
 					<i class="fas fa-bars"></i>
 				</button>
-				<div class="collapse navbar-collapse" id="navbarResponsive">
-					<ul class="navbar-nav ml-auto py-4 py-lg-0">
-						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-						<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
-					</ul>
-				</div>
+
+				<?php
+					wp_nav_menu(
+						array(
+							'menu_id'        => 'primary-menu',
+							'container'         => "div",
+							'container_class'   => "collapse navbar-collapse",
+							'container_id'      => "navbarResponsive",
+							'menu_class'      => "navbar-nav ml-auto py-4 py-lg-0",
+							'depth'       => "3",
+							'fallback_cb'       => "WP_Bootstrap_Navwalker::fallback",
+							'walker'       => new WP_Bootstrap_Navwalker(),
+							'theme_location' => 'menu-1',
+						)
+					);
+				?>
 			</div>
 		</nav>
-	</div>
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$blogsia_description = get_bloginfo( 'description', 'display' );
-			if ( $blogsia_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $blogsia_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'blogsia' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	</header>
