92238ae24fde9fab3644b686b7237e08a65cb855
monzuralam <monzur707@gmail.com>
2022-06-13 18:02:48 +0600

    update customizer path & version

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function blogsia_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function blogsia_customize_preview_js() {
-	wp_enqueue_script( 'blogsia-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'blogsia-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), BLOGSIA_VERSION, true );
 }
 add_action( 'customize_preview_init', 'blogsia_customize_preview_js' );
