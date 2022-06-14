46d9167c9de8da89b36d8f1936e47cffddeb4347
monzuralam <monzur707@gmail.com>
2022-06-14 09:29:12 +0600

    page dynamic

--- page.php
+++ page.php
@@ -15,24 +15,27 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main mb-4">
+		<div class="container px-4 px-lg-5">
+			<div class="row gx-4 gx-lg-5 justify-content-center">
+				<div class="col-md-10 col-lg-8 col-xl-7">
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+						get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+					endwhile; // End of the loop.
+					?>
+				</div>
+			</div>
+		</div>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
