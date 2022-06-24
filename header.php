50d0693da9a972ead489608fff603ea88b9a59f7
monzuralam <monzur707@gmail.com>
2022-06-24 19:25:28 +0600

    theme check error has been fix

--- header.php
+++ header.php
@@ -159,4 +159,18 @@
 		</div>
 	</section>
 	<?php
+	}else{
+	?>
+	<section class="hero-section masthead">
+		<div class="container position-relative px-4 px-lg-5">
+			<div class="row gx-4 gx-lg-5 justify-content-center">
+				<div class="col-md-10 col-lg-8 col-xl-7">
+					<div class="site-heading">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	<?php
 	}
