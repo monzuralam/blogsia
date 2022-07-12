a087eb50f27cb52f7862a4240852e134b9a5a79d
monzuralam <monzur707@gmail.com>
2022-07-12 10:41:03 +0600

    col size updated

--- page.php
+++ page.php
@@ -18,7 +18,7 @@ get_header();
 	<main id="primary" class="site-main mb-4">
 		<div class="container px-4 px-lg-5">
 			<div class="row gx-4 gx-lg-5 justify-content-center">
-				<div class="col-md-10 col-lg-8 col-xl-7">
+				<div class="col-md-12 col-lg-12 col-xl-12">
 					<?php
 
 						while ( have_posts() ) : the_post();
